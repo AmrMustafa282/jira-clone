0c98fa4c19ef01e4cea8a2716b696d9d706d3428
AmrMustafa282 <amrdarwish115@gmail.com>
2025-03-18 02:38:02 +0200

    refactor:customizing button & input components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import TestComp from "@/features/Test";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
  return (
   <div>
-   <TestComp />
+   <Button disabled>Primary</Button>
+   <Button variant={"secondary"}>Secondary</Button>
+   <Button variant={"destructive"}>Destructive</Button>
+   <Button variant={"ghost"}>Ghost</Button>
+   <Button variant={"muted"}>Muted</Button>
+   <Button variant={"outline"}>Outline</Button>
+   <Button variant={"teritary"}>Teritary</Button>
   </div>
  );
 }
--- src/features/Test.tsx
+++
@@ -1,5 +0,0 @@
-const TestComp = () => {
-  return ( <div className="text-red-600">sami barhoma</div> );
-}
-
-export default TestComp;
