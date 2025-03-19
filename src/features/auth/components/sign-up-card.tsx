5876bb16bcd30100eed1bfe6d3a35f9501d57f6c
AmrMustafa282 <amrdarwish115@gmail.com>
2025-03-19 02:50:49 +0200

    feat:create auth apis

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -23,24 +23,22 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import Link from 'next/link'
-
-const formSchema = z.object({
-  name: z.string().trim().min(1, 'Required'),
-  email: z.string().email(),
-  password: z.string().min(8, 'Password must be at least 8 characters'),
-})
+import { registerSchema } from '../schemas'
+import { useSignUp } from '../api/use-signup'
 
 const SignUpCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { mutate } = useSignUp()
+
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
     defaultValues: {
       name: '',
       email: '',
       password: '',
     },
   })
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+  const onSubmit = (values: z.infer<typeof registerSchema>) => {
+    mutate({ json: values })
   }
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
