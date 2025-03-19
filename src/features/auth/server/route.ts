d14c6bff630509dff6db9f824af6189374ce9bd8
AmrMustafa282 <amrdarwish115@gmail.com>
2025-03-19 17:04:38 +0200

    feat:setup appwrite and added auth apis

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,20 +1,51 @@
 import { Hono } from 'hono'
 import { zValidator } from '@hono/zod-validator'
 import { loginSchema, registerSchema } from '../schemas'
+import { createAdminClient } from '@/lib/appwirte'
+import { ID } from 'node-appwrite'
+import { setCookie, deleteCookie } from 'hono/cookie'
+import { AUTH_COOKIE } from '../constants'
 
 const app = new Hono()
   .post('/login', zValidator('json', loginSchema), async (c) => {
     const { email, password } = c.req.valid('json')
 
-    console.log({ email, password })
+    const { account } = await createAdminClient()
+    const session = await account.createEmailPasswordSession(email, password)
 
-    return c.json({ email, password })
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30,
+    })
+
+    return c.json({ success: true })
   })
   .post('/register', zValidator('json', registerSchema), async (c) => {
     const { name, email, password } = c.req.valid('json')
-    console.log({ name, email, password })
 
-    return c.json({ name, email, password })
+    const { account } = await createAdminClient()
+    await account.create(ID.unique(), email, password, name)
+
+    const session = await account.createEmailPasswordSession(email, password)
+
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30,
+    })
+
+    return c.json({ success: true })
+  })
+  .post('/logout', async (c) => {
+    deleteCookie(c, AUTH_COOKIE)
+    const { account } = await createAdminClient()
+    await account.deleteSession('current')
+    return c.json({ success: true })
   })
 
 export default app
